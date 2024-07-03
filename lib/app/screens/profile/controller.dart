39cce5ce359a1bbab347aaa78c493cf3ca8818d6
Seungpyo Suh <sspyorea@gmail.com>
2024-07-03 21:02:27 +0900

    fix: signOut

--- lib/app/screens/profile/controller.dart
+++ lib/app/screens/profile/controller.dart
@@ -1,8 +1,10 @@
 import 'package:get/get.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:wesee/app/routes/routes.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 final supabase = Supabase.instance.client;
+final GoogleSignIn _googleSignIn = GoogleSignIn();
 
 class ProfileController extends GetxController {
   final user = supabase.auth.currentUser;
@@ -11,6 +13,8 @@ class ProfileController extends GetxController {
 
   Future<void> signOut() async {
     await supabase.auth.signOut();
+    await _googleSignIn.signOut();
+
     Get.offAllNamed(Routes.HOME);
   }
 }
